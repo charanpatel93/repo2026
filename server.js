ea512693fcb4ec44d716eae1cac69285faf0c728
charanpatel93 <charansaiakula2151@gmail.com>
2026-02-05 19:30:58 +0530

    ok tested

--- server.js
+++ server.js
@@ -118,8 +118,10 @@ app.post("/login", upload.none(), async (req, res) => {
   });
 });
 const path = require("path");
+
 app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("*", (req, res) => {
+
+app.get("/*", (req, res) => {
   res.sendFile(
     path.join(__dirname, "client", "build", "index.html")
   );
